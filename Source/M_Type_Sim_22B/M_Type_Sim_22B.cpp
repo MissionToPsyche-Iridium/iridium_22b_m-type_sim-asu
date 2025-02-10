e91a83215edcb047bac2d782c3da85b52a0f9a23
Sam Cahill <117058863+SamCahill@users.noreply.github.com>
2025-02-10 01:00:07 -0700

    Implement asteroid and gravity
    Add the gravity and asteroid upscaling implementation from previous sprints. Reduced asteroid mesh tessellation quality to reduce file size.

---
+++ Source/M_Type_Sim_22B/M_Type_Sim_22B.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "M_Type_Sim_22B.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, M_Type_Sim_22B, "M_Type_Sim_22B" );
